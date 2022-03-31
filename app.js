2aa2d00516c86fea376d54df4422152ad0fd4219
root <root@DESKTOP-D35VALO.localdomain>
2022-03-31 10:03:59 +0200

    test

--- app.js
+++ app.js
@@ -8,15 +8,17 @@ const session = require('express-session');
 const passport = require("passport");
 const multer = require("multer");
 const cloudinary = require("cloudinary");
-const {CloudinaryStorage} = require("multer-storage-cloudinary");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const fileupload = require('express-fileupload')
 
 //passport config:
 require('./config/passport')(passport)
-//mongoose
-mongoose.connect('',{useNewUrlParser: true, useUnifiedTopology : true})
-.then(() => console.log('connected,,'))
-.catch((err)=> console.log(err));
+    //mongoose
+mongoose.connect('', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('connected,,'))
+    .catch((err) => console.log(err));
+
+
 
 
 //CLOUDINARY
@@ -25,7 +27,7 @@ cloudinary.config({
     cloud_name: "",
     api_key: "",
     api_secret: ""
-  });
+});
 
 // const storage = new CloudinaryStorage({
 // cloudinary: cloudinary,
@@ -36,37 +38,37 @@ cloudinary.config({
 // const parser = multer({ storage: storage });
 
 //EJS
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.use(expressEjsLayout);
 //BodyParser
-app.use(express.urlencoded({extended : false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
-//express session
+    //express session
 app.use(session({
-    secret : 'secret',
-    resave : true,
-    saveUninitialized : true
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
 }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
-app.use((req,res,next)=> {
+app.use((req, res, next) => {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
-    res.locals.error  = req.flash('error');
+    res.locals.error = req.flash('error');
     next();
-    })
+})
 
-app.use(fileupload({useTempFiles: true}))
+app.use(fileupload({ useTempFiles: true }))
 app.use("/static", express.static("public"));
 
 
 
 //Routes
-app.use('/',require('./routes/index'));
-app.use('/users',require('./routes/users'));
-app.use('/profiles',require('./routes/profiles'));
-app.use('/posts',require('./routes/posts'));
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+app.use('/profiles', require('./routes/profiles'));
+app.use('/posts', require('./routes/posts'));
 
 
 
