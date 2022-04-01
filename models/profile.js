1d8eccd8e08a1f8b697372e8dac0cbecfee6433c
dburgiss <dylan.burgisser@hotmail.com>
2022-04-01 11:29:27 +0200

    added creation and activities page + info to db

--- models/profile.js
+++ models/profile.js
@@ -1,28 +1,53 @@
 const mongoose = require('mongoose');
 const ProfileSchema  = new mongoose.Schema({
-  username :{
-      type  : String,
-      required : true
-  } ,
-  favorite_animal :{
-    type  : String,
-    required : true
-  } ,
-  popularity_level :{
-    type  : Number,
-    required : true
-  } ,
+  name: {
+    type: String,
+    required: true
+  },
+
+  bio: {
+    type: String,
+    required: false
+  },
+  // image: {
+  //   data: Buffer,
+  //   contentType: String,
+  //   required: true
+  // },
+  
   date :{
     type : Date,
     default : Date.now
   },
-  posts : [
+
+  // comments: [
+  //   {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: "Comment"
+  //   }
+  // ],
+
+  activities: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Post"
+      ref: "Activity"
     }
-  ]
+  ],
+
+  // ratings: [
+  //   {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: "Rating"
+  //   }
+  // ],
+
+  // posts: [
+  //   {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: "Post"
+  //   }
+  // ],
 });
-const Profile = mongoose.model('Profile',ProfileSchema);
+const Profile = mongoose.model('Profile', ProfileSchema);
 
 module.exports = {Profile, ProfileSchema};
