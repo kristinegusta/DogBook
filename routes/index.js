cb5db4f648b7699bf0f4c23e9ad7a878428aa645
OmerAslani <omersatelit21@gmail.com>
2022-04-11 12:45:27 +0200

    review page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { ensureAuthenticated } = require('../config/auth')
+const { ensureAuthenticated } = require("../config/auth");
 const Profile = require("../models/profile").Profile;
 // const Post = require("../models/post").Post;
 const Activity = require("../models/activitiy").Activity;
@@ -9,40 +9,78 @@ const Review = require("../models/review").Review;
 const TrainerProfile = require("../models/trainerProfile").TrainerProfile;
 
 //test page
-router.get('/test', (req, res) => {
-    res.render("oldActivity_post")
-})
-
+router.get("/test", (req, res) => {
+    res.render("oldActivity_post");
+});
 
 // Activity Review page
 router.get("/ActivityReview", (req, res) => {
-    let activityId = req.url.split("?")
+    let activityId = req.url.split("?");
     // res.render("activityReview")
-    renderActivityReview(res, activityId[1])
-})
-const renderActivityReview = async function (res, id) {
-    let activity = await getActivityFromDB(id)
-    let reviews = await getReviewFromDB(id)
-    console.log(reviews)
+    renderActivityReview(res, activityId[1], req);
+});
+const renderActivityReview = async function(res, id, req) {
+    let activity = await getActivityFromDB(id);
+    let reviews = await getReviewFromDB(id);
     res.render("activityReview", {
         activity: activity,
-        reviews: reviews
+        reviews: reviews,
+        user: req.user
     });
-}
+};
+
+router.get("/trainerReview", (req, res) => {
+    let trainerId = req.url.split("?");
+    renderTrainerReview(res, trainerId[1], req);
+})
+
+
+const renderTrainerReview = async function(res, id, req) {
+    let trainer = await getTrainersFromDB(id);
+    let reviews = await getReviewFromDB(id);
+    res.render("trainerReview", {
+        trainer: trainer,
+        reviews: reviews,
+        user: req.user
+    });
+};
+
+
+const getTrainersFromDB = async() => {
+    let trainer = [];
+    const cursor = await Trainer.find({});
+
+    for (let i = 0; i < cursor.length; i++) {
+        let doc = cursor[i];
+        let info = {};
+
+        info.trainerId = doc._id;
+        info.name = doc.name;
+        info.street = doc.street;
+        info.city = doc.city;
+        info.country = doc.country;
+        info.phone = doc.phone;
+        info.email = doc.email;
+        info.website = doc.website;
+
+        trainer.push(info);
+    }
+    return trainer;
+};
 
 // Activity About Page
 router.get("/ActivityAbout", (req, res) => {
-    let activityId = req.url.split("?")
-    renderActivityAbout(res, activityId[1])
-})
-const renderActivityAbout = async function (res, id) {
-    let activity = await getActivityFromDB(id)
-
+    let activityId = req.url.split("?");
+    renderActivityAbout(res, activityId[1], req);
+});
+const renderActivityAbout = async function(res, id, req) {
+    let activity = await getActivityFromDB(id);
     res.render("activities-about", {
-        activity: activity
+        activity: activity,
+        user: req.user,
     });
-}
-const getReviewFromDB = async (id) => {
+};
+const getReviewFromDB = async(id) => {
     let reviews = [];
     const cursor = await Activity.find({ _id: id });
     const review = await Review.find({ _id: cursor[0].reviews });
@@ -56,73 +94,67 @@ const getReviewFromDB = async (id) => {
             rating: "",
             time: "",
             authorName: "",
-            authorImg: ""
-        }
+            authorImg: "",
+        };
 
-
-        info.reviewId = doc._id
-        info.description = doc.description
-        info.rating = doc.rating
-        let date = doc.date.toString().split("GMT")
-        info.time = date[0].trim()
+        info.reviewId = doc._id;
+        info.description = doc.description;
+        info.rating = doc.rating;
+        let date = doc.date.toString().split("GMT");
+        info.time = date[0].trim();
 
         const result = await Profile.find({ reviews: doc._id });
-        info.authorName = result[0].name
-        info.authorImg = result[0].url
-        reviews.push(info)
+        info.authorName = result[0].name;
+        info.authorImg = result[0].url;
+        reviews.push(info);
     }
     return reviews;
-}
+};
 
-const getActivityFromDB = async (id) => {
+const getActivityFromDB = async(id) => {
     let activities = [];
 
     const cursor = await Activity.find({ _id: id });
-
+    console.log(cursor)
     for (let i = 0; i < cursor.length; i++) {
         let doc = cursor[i];
 
-        let info = {
-            activityId: "",
-            activityName: "",
-            description: "",
-            location: "",
-            time: "",
-            authorName: "",
-        }
+        let info = {};
 
-        info.activityId = doc._id
-        info.activityName = doc.name
-        info.description = doc.description
-        info.location = doc.location
+        info.activityId = doc._id;
+        info.activityName = doc.name;
+        info.description = doc.description;
+        info.location = doc.location;
+        info.url = doc.url;
 
-        let date = doc.date.toString().split("GMT")
-        info.time = date[0].trim()
+        let date = doc.date.toString().split("GMT");
+        info.time = date[0].trim();
 
         const result = await Profile.find({ activities: doc._id });
-        info.authorName = result[0].name
-        activities.push(info)
+        info.authorName = result[0].name;
+        info.authorImg = result[0].url;
+        activities.push(info);
     }
     return activities;
-}
+};
 
 //landing page
-router.get('/', (req, res) => {
-    res.render('index');
-})
+router.get("/", (req, res) => {
+    res.render("index");
+});
 // dashboard
-router.get('/dashboard', ensureAuthenticated, (req, res) => {
+router.get("/dashboard", ensureAuthenticated, (req, res) => {
     if (!req.user.profile) {
-        res.render('dashboard', {
-            user: req.user
+        res.render("dashboard", {
+            user: req.user,
         });
     } else {
         // renderDashboardWithPosts(req, res)
         // renderAllActivities()
-        res.redirect("activities")
+        res.redirect("activities");
     }
-})
-// trainer dashboard 
+});
+// trainer dashboard
 router.get("/trainerCreate", ensureAuthenticated, (req, res) => {
     if (!req.user.profile) {
         res.render("trainerCreate", {
@@ -135,11 +167,11 @@ router.get("/trainerCreate", ensureAuthenticated, (req, res) => {
 
 /* EVERYTHING ACTIVITIES RELATED */
 //rendering activities page
-router.get('/activities', (req, res) => {
-    renderAllActivities(res)
-})
+router.get("/activities", (req, res) => {
+    renderAllActivities(res, req);
+});
 
-const getActivitiesFromDB = async () => {
+const getActivitiesFromDB = async() => {
     let activities = [];
     const cursor = await Activity.find({});
     for (let i = 0; i < cursor.length; i++) {
@@ -149,8 +181,8 @@ const getActivitiesFromDB = async () => {
         const review = await Review.find({ _id: cursor[i].reviews });
         for (let i = 0; i < review.length; i++) {
             let doc = review[i];
-            let rating = doc.rating
-            reviews.push(rating)
+            let rating = doc.rating;
+            reviews.push(rating);
         }
 
         let info = {
@@ -160,50 +192,56 @@ const getActivitiesFromDB = async () => {
             location: "",
             time: "",
             authorName: "",
+            authorImg: "",
             rating: "",
             reviews: "",
-        }
+        };
 
         if (reviews.length === 0) {
-            info.rating = 0
+            info.rating = 0;
         } else {
-            let total = 0
+            let total = 0;
             for (let rating of reviews) {
-                total += rating
+                total += rating;
             }
-            info.rating = Math.round((total/(reviews.length))*10)/10
+            info.rating = Math.round((total / reviews.length) * 10) / 10;
         }
-        info.reviews = reviews.length
-        info.activityId = doc._id
-        info.activityName = doc.name
-        info.description = doc.description
-        info.location = doc.location
+        info.reviews = reviews.length;
+        info.activityId = doc._id;
+        info.activityName = doc.name;
+        info.description = doc.description;
+        info.location = doc.location;
+        info.url = doc.url;
 
-        let date = doc.date.toString().split("GMT")
-        info.time = date[0].trim()
+        let date = doc.date.toString().split("GMT");
+        info.time = date[0].trim();
+
 
         const result = await Profile.find({ activities: doc._id });
-        info.authorName = result[0].name
-        activities.push(info)
+        info.authorName = result[0].name;
+        info.authorImg = result[0].url;
+
+        activities.push(info);
     }
     return activities;
-}
+};
 
+const renderAllActivities = async function(res, req) {
+    let activities = await getActivitiesFromDB();
 
-const renderAllActivities = async function (res) {
-    let activities = await getActivitiesFromDB()
     res.render("activities", {
-        activities: activities
+        activities: activities,
+        user: req.user,
     });
-}
+};
 
 /* EVERYTHING TRAINER RELATED */
 //trainers page
 router.get("/trainers", (req, res) => {
-    renderAllTrainers(res);
+    renderAllTrainers(res, req);
 });
 
-const notScrappedGetTrainersFromDB = async () => {
+const notScrappedGetTrainersFromDB = async() => {
     let trainers = [];
     const cursor = await TrainerProfile.find({});
 
@@ -220,14 +258,14 @@ const notScrappedGetTrainersFromDB = async () => {
         info.website = doc.website;
         info.bio = doc.bio;
         info.time = doc.date;
+        info.url = doc.url;
         trainers.push(info);
-
     }
 
     return trainers;
 };
 
-const getTrainerFromDB = async () => {
+const getTrainerFromDB = async() => {
     let scrappedTrainers = [];
     const cursor = await Trainer.find({});
 
@@ -248,13 +286,14 @@ const getTrainerFromDB = async () => {
     }
     return scrappedTrainers;
 };
-const renderAllTrainers = async function (res) {
+const renderAllTrainers = async function(res, req) {
     let scrappedTrainers = await getTrainerFromDB();
     let trainers = await notScrappedGetTrainersFromDB();
 
     res.render("trainer", {
         trainers: trainers,
         scrappedTrainers: scrappedTrainers,
+        user: req.user
     });
 };
 
