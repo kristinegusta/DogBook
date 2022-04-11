c74d903c9b89b8f579073d3bfb372113994ade5b
Antoine De Poorter <antoine.depoorter@outlook.com>
2022-04-11 14:45:12 +0200

    css

--- app.js
+++ app.js
@@ -7,30 +7,27 @@ const flash = require("connect-flash");
 const session = require("express-session");
 const passport = require("passport");
 const fileupload = require("express-fileupload");
-const dotenv = require('dotenv').config();
+const dotenv = require("dotenv").config();
 
 //passport config:
 require("./config/passport")(passport);
 //fileupload co
-router.use(fileupload({ useTempFiles: true }))
+router.use(fileupload({ useTempFiles: true }));
 
 //mongoose
 // Dylan cluster
-const dbURI = process.env.DB_URI
+const dbURI = process.env.DB_URI;
 // Antoine cluster
 // const dbPassword = "ppp111ppp111";
 // const dbURI = `mongodb+srv://antoine:${dbPassword}@cluster0.h3l4q.mongodb.net/dogbook_database?retryWrites=true&w=majority`;
 
 mongoose
-    .connect(dbURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log("connected,,"))
-    .catch((err) => console.log(err));
-
-
-
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connected,,"))
+  .catch((err) => console.log(err));
 
 //EJS
 app.set("view engine", "ejs");
@@ -38,23 +35,23 @@ app.use(expressEjsLayout);
 //BodyParser
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-router.use(fileupload({ useTempFiles: true }))
+router.use(fileupload({ useTempFiles: true }));
 //express session
 app.use(
-    session({
-        secret: "secret",
-        resave: true,
-        saveUninitialized: true,
-    })
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 app.use((req, res, next) => {
-    res.locals.success_msg = req.flash("success_msg");
-    res.locals.error_msg = req.flash("error_msg");
-    res.locals.error = req.flash("error");
-    next();
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
 });
 
 app.use(fileupload({ useTempFiles: true }));
@@ -65,7 +62,7 @@ app.use("/", require("./routes/index"));
 app.use("/users", require("./routes/users"));
 app.use("/profiles", require("./routes/profiles"));
 app.use("/posts", require("./routes/posts"));
-app.use("/about", require("./routes/about"))
+app.use("/about", require("./routes/about"));
 // app.use("/trainer", require("./routes/trainer"));
 // app.use("/activity", require("./routes/activity"));
 
